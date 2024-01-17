1e97e904588889ad5e523facd7bae641778c53f5
DefenetlyNotSimon <simon21.blum@gmail.com>
2024-01-17 20:12:30 +0100

    Reorganize project structure and update CMakeLists
    Project structure has been reorganized into more clear and logical divisions among entities, handlers, and tasks. Various CMakeLists.txt files have also been updated to accommodate these changes. The commit aims to improve readability, maintainability, and scalability of the project.

---
+++ src/handlers/commandHandler.cpp
@@ -0,0 +1,4 @@
+#include "commandHandler.hpp"
+
+namespace myClasses {
+} // myClasses
---
+++ src/handlers/commandHandler.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace myClasses {
+
+    class commandHandler {
+
+    };
+
+} // myClasses
