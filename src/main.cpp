0da2039e110e7694dc335b9160e83ebaba7a1432
DefenetlyNotSimon <simon21.blum@gmail.com>
2024-01-17 21:54:51 +0100

    Update main.cpp to handle program start up conditions
    Refactored the main.cpp file to handle conditions in which the program is being started with or without commands. Logger instances have been added to provide insight into.

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,40 @@
-#include <sqlite3.h>
-
 #include <iostream>
 #include <string>
 #include <vector>
 
-#include "tasks/temperature.hpp"
-#include "utils.hpp"
-#include "handlers/projectHandler.hpp"
+#include "TemperatureTask.hpp"
+#include "Utils.hpp"
+#include "Logger.hpp"
+#include "CommandHandler.hpp"
 
-using namespace myUtils;
+using namespace myLibs;
 using namespace myClasses;
-using namespace myProjectHandler;
+
+std::vector<std::string> projects = {"Temperatures", "project2"};
 
 int main(int argc, char *argv[]) {
-    std::vector<std::string> projects = {"Temperatures", "project2"};
+    if(argc > 1) {
+        Logger::getInstance().log("Program started with commands!", "Info");
+        CommandHandler cmdHandler(argc, argv);
+        Logger::getInstance().log("Program ended after CommandHandler execution!", "Info");
+        return 0;
+    }
+
+    Logger::getInstance().log("Program started without commands", "Info");
     std::cout << "Hello there!\nPress any button to continue!\n";
     std::cin.ignore();
     std::cout << "Which project do you want to open?\n";
+
     int i = 1;
     for (const std::string &project: projects) {
         std::cout << i << " - " << project << "\n";
         ++i;
     }
-    std::string input = "";
-    bool correctInput = false;
+    Logger::getInstance().log("Waiting for user input!", "Debug");
     int inputInt = Utils::inputInt();
     switch (inputInt) {
         case 1: {
-            myTemperature::Temperature temperatureProject =
-                    myTemperature::Temperature(true);
+            myTasks::Temperature(true);
             break;
         }
         case 2: {
@@ -38,5 +44,6 @@ int main(int argc, char *argv[]) {
             std::cout << "This project does not exist!\n";
         }
     }
+    Logger::getInstance().log("Program ended after project execution!", "Info");
     return 0;
 }
