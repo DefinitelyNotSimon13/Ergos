01631ba71c495f4c17ce8ae0f7f7cb6aad424a5f
DefenetlyNotSimon <simon21.blum@gmail.com>
2024-01-17 21:56:23 +0100

    Rename 'logger' to 'Logger'
    The codebase has been updated to follow naming convention standards. As a result, all instances of 'logger' have been renamed to 'Logger'. This includes file names, class names, and method calls.

--- include/Logger.cpp
+++ include/Logger.cpp
@@ -1,39 +1,39 @@
-#include "logger.hpp"
+#include "Logger.hpp"
 #include <chrono>
 #include <iomanip>
 
 namespace myLibs {
 
-    logger::logger() {
+    Logger::Logger() {
         auto now = std::chrono::system_clock::now();
         auto time = std::chrono::system_clock::to_time_t(now);
         std::stringstream filename;
         filename << std::string(SOURCE_DIR) << "/logs/log_" << std::put_time(std::localtime(&time), "%Y-%m-%d")
                  << ".txt";
-        logger::logFile.open(filename.str(), std::ios::app);
+        Logger::logFile.open(filename.str(), std::ios::app);
     }
 
-    logger::~logger() {
-        logger::logFile.close();
+    Logger::~Logger() {
+        Logger::logFile.close();
     }
 
-    logger &logger::getInstance() {
-        static logger instance;
+    Logger &Logger::getInstance() {
+        static Logger instance;
         return instance;
     }
 
     void
-    logger::log(const std::string &message, const std::string &filename, const int &line, const std::string &function,
+    Logger::log(const std::string &message, const std::string &filename, const int &line, const std::string &function,
                 const std::string &type) {
-        logger::logFile << getTimestamp() << type << " - " << filename << ":" << line << " " << function << " "
+        Logger::logFile << getTimestamp() << type << " - " << filename << ":" << line << " " << function << " "
                         << message << std::endl;
     }
 
-    void logger::log(const std::string &message, const std::string &type) {
-        logger::logFile << getTimestamp() << type << " - " << message << std::endl;
+    void Logger::log(const std::string &message, const std::string &type) {
+        Logger::logFile << getTimestamp() << type << " - " << message << std::endl;
     }
 
-    std::string logger::getTimestamp() {
+    std::string Logger::getTimestamp() {
         auto now = std::chrono::system_clock::now();
         auto time = std::chrono::system_clock::to_time_t(now);
 
--- include/Logger.hpp
+++ include/Logger.hpp
@@ -4,17 +4,17 @@
 
 namespace myLibs {
 
-    class logger {
+    class Logger {
     public:
-        static logger& getInstance();
+        static Logger& getInstance();
         void log(const std::string& message, const std::string& filename, const int& line, const std::string& function, const std::string& type = "INFO");
         void log(const std::string& message, const std::string& type = "INFO");
 
-        logger(const logger&)= delete;
-        logger& operator=(const logger&)= delete;
+        Logger(const Logger&)= delete;
+        Logger& operator=(const Logger&)= delete;
     private:
-        logger();
-        ~logger();
+        Logger();
+        ~Logger();
         static std::string getTimestamp();
         std::ofstream logFile;
     };
