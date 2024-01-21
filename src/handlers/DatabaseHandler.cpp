8b93fcae7792def66bae50ae4fa607ab621509a1
DefenetlyNotSimon <simon21.blum@gmail.com>
2024-01-21 14:07:40 +0100

    Remove Sqlite3 dependencies
    Remove Sqlite3 dependencies to prepare for move to MongoDB.

--- src/handlers/DatabaseHandler.cpp
+++ src/handlers/DatabaseHandler.cpp
@@ -2,17 +2,9 @@
 
 namespace myClasses {
     DatabaseHandler::DatabaseHandler(const char *filename) {
-        int statusCode = sqlite3_open(filename, &this->db);
     }
 
     DatabaseHandler::~DatabaseHandler() {
-        sqlite3_close(db);
-    }
 
-    const sqlite3 *DatabaseHandler::getDatabase() const { return this->db; }
-
-    int16_t DatabaseHandler::executeCommand(const char *command) const {
-        return (int16_t) sqlite3_exec(this->db, command, nullptr, nullptr,
-                                      nullptr);
     }
 }  // namespace myClasses
--- src/handlers/DatabaseHandler.hpp
+++ src/handlers/DatabaseHandler.hpp
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <cstdint>
-#include "sqlite3.h"
 
 namespace myClasses {
     class DatabaseHandler {
@@ -10,13 +9,8 @@ namespace myClasses {
 
         explicit DatabaseHandler(const char *filename);
 
-        [[nodiscard]] const sqlite3 *getDatabase() const;
-
-        int16_t executeCommand(const char *command) const;
-
         ~DatabaseHandler();
 
     private:
-        sqlite3 *db{};
     };
 }  // namespace myClasses
